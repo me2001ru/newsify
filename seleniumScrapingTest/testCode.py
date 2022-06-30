5b41c849f628ad91c0c19aa7214bec1f871d6288
Mekasha Tadious <mekasha.tadious@gmail.com>
2022-06-30 11:52:32 +0200

    my selenium bug

---
+++ seleniumScrapingTest/testCode.py
@@ -0,0 +1,32 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.techwithtim.net/")
+
+print(driver.title)
+
+
+search = driver.find_element(By.NAME, "s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    articles = main.find_elements(By.TAG_NAME, 'article')
+    for article in articles:
+        header = article.find_element(By.CLASS_NAME, "entry-summary")
+        print(header.text)
+finally:
+    driver.quit()
+
+
